7b5c2809dafc4ee06defb9c90e83d1f135f3e30e
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-10-06 16:08:34 -0400

    LabNumber9 (First Commit)

---
+++ LabNumber9/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber9
+{
+    class Circle
+    {
+        //Variable
+
+        private double radius;
+
+         //Property
+        public double Radius
+        {
+            set {radius = value;}
+            get { return radius; }
+
+        }
+       
+        //Methods
+        public double GetArea()
+        {
+            double pi = Math.PI;
+            double area = pi * radius * radius;
+            return area;
+
+        }
+
+        public double GetCircumference()
+        {
+         
+            double pi = Math.PI;
+            double circumference = 2 * pi * radius;
+            return circumference;
+        }
+
+
+
+
+    }
+}
---
+++ LabNumber9/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Serena Tindle-- LabNumber9 (Circumference and Area of Radius)
+
+            bool ContinueProgram = true;
+            while (ContinueProgram == true)
+            {
+
+                double radius; //Variable
+                Circle RadiusCalc = new Circle(); //create an object
+
+                Console.WriteLine("Welcome to the Circle Tester"); //prompt
+                Console.WriteLine("Enter radius:");
+                radius = double.Parse(Console.ReadLine());
+                while (!double.TryParse(Console.ReadLine(), out radius))
+                {
+                    {
+                        Console.WriteLine("Please enter valid data");
+                    }
+                    
+                }
+               
+               
+                RadiusCalc.Radius = radius;
+                Console.WriteLine("Circumference:" + RadiusCalc.GetCircumference());
+                Console.WriteLine("Area:"+RadiusCalc.GetArea());//call method for area
+
+                string UserOption; //Continue program or exit?
+                Console.WriteLine("Continue? (y/n)");
+                UserOption = Console.ReadLine();
+
+                if (UserOption == "N" || UserOption == "No" || UserOption == "no" || UserOption == "n")
+                {
+                    ContinueProgram = false;
+                    Console.WriteLine("Goodbye. You created two circle object(s).");
+
+                }
+
+                Console.ReadKey();
+
+
+        }
+        }
+    }
+}
